801b3e3e7157270586f7254d0cebd1071d2893fc
Avrahm <avrahmk@me.com>
2022-09-13 10:24:47 -0400

    add aws to project

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-import './shim';
+import { Amplify } from "aws-amplify";
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import awsConfig from "./frontend/src/aws-exports";
+import "./shim";
+Amplify.configure(awsConfig);
 
 AppRegistry.registerComponent(appName, () => App);
